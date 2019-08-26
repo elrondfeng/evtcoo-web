c542b0054d5bb53c3df1e415b019335362e68674
Tao Feng <tao.feng@duke-energy.com>
2019-08-25 22:51:36 -0400

    first commit

--- src/app/service/httpclient.service.ts
+++ src/app/service/httpclient.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-export class Employee{
+export class Employee {
   constructor(
-    public empId:string,
-    public name:string,
-    public designation:string,
-    public salary:string,
+    public empId: string,
+    public name: string,
+    public designation: string,
+    public salary: string,
   ) {}
 }
 
@@ -16,21 +16,19 @@ export class Employee{
 export class HttpClientService {
 
   constructor(
-    private httpClient:HttpClient
-  ) { 
-     }
+    private httpClient: HttpClient
+  ) {}
 
-     getEmployees()
-  {
-    console.log("test call");
+     getEmployees() {
+    console.log('test call');
     return this.httpClient.get<Employee[]>('http://localhost:8080/employees');
   }
 
   public deleteEmployee(employee) {
-    return this.httpClient.delete<Employee>("http://localhost:8080/employees" + "/"+ employee.empId);
+    return this.httpClient.delete<Employee>('http://localhost:8080/employees' + '/' + employee.empId);
   }
 
   public createEmployee(employee) {
-    return this.httpClient.post<Employee>("http://localhost:8080/employees", employee);
+    return this.httpClient.post<Employee>('http://localhost:8080/employees', employee);
   }
 }
